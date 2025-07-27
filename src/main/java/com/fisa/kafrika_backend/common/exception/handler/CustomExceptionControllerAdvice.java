ae24a9289c80b85f1d8fe417427a715beefc4db3
hyunn522 <tjgus52213@gmail.com>
2025-07-27 19:22:46 +0900

    feat: HTTP 요청에 대한 Exception 추가

---
+++ src/main/java/com/fisa/kafrika_backend/common/exception/handler/CustomExceptionControllerAdvice.java
@@ -0,0 +1,26 @@
+package com.fisa.kafrika_backend.common.exception.handler;
+
+import com.fisa.kafrika_backend.common.exception.CustomException;
+import com.fisa.kafrika_backend.common.response.BaseErrorResponse;
+import jakarta.annotation.Priority;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@Priority(0)
+@RestControllerAdvice
+public class CustomExceptionControllerAdvice {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<BaseErrorResponse> handle_JwtBadRequestException(CustomException e) {
+        log.error("[handle_JwtBadRequestException]", e);
+        BaseErrorResponse errorResponse = new BaseErrorResponse(e.getExceptionStatus(), e.getMessage());
+        return ResponseEntity.status(e.getExceptionStatus().getStatus()).body(errorResponse);
+    }
+
+}
