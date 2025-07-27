12eaf5c816b633eb9f7290bc0e4d26f40806fd34
hyunn522 <tjgus52213@gmail.com>
2025-07-27 20:18:53 +0900

    fix: 에러 메시지 중복 출력 개선

--- src/main/java/com/fisa/kafrika_backend/common/util/BindingResultUtils.java
+++ src/main/java/com/fisa/kafrika_backend/common/util/BindingResultUtils.java
@@ -1,12 +1,15 @@
 package com.fisa.kafrika_backend.common.util;
 
+import java.util.stream.Collectors;
 import org.springframework.validation.BindingResult;
 
 public class BindingResultUtils {
 
     public static String getErrorMessage(BindingResult bindingResult) {
-        StringBuilder errorMessage = new StringBuilder();
-        bindingResult.getAllErrors().forEach(error -> errorMessage.append(error.getDefaultMessage()).append(". "));
-        return errorMessage.toString();
+        return bindingResult.getAllErrors().stream()
+                .map(error -> error.getDefaultMessage())
+                .distinct()
+                .collect(Collectors.joining(". ")) + ".";
     }
+
 }
