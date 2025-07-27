3a032d00baa6f28339d7a3b51b5ceeaa62daeace
hyunn522 <tjgus52213@gmail.com>
2025-07-27 19:42:17 +0900

    feat: HTTP 요청의 body 검증을 위한 라이브러리 및 util 추가

---
+++ src/main/java/com/fisa/kafrika_backend/common/util/BindingResultUtils.java
@@ -0,0 +1,12 @@
+package com.fisa.kafrika_backend.common.util;
+
+import org.springframework.validation.BindingResult;
+
+public class BindingResultUtils {
+
+    public static String getErrorMessage(BindingResult bindingResult) {
+        StringBuilder errorMessage = new StringBuilder();
+        bindingResult.getAllErrors().forEach(error -> errorMessage.append(error.getDefaultMessage()).append(". "));
+        return errorMessage.toString();
+    }
+}
