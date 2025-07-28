ec4d8454fae56616c1a222b8a0127e147244cf06
hyunn522 <tjgus52213@gmail.com>
2025-07-28 18:46:18 +0900

    feat: 디폴트 채팅방 이름 환경변수로 주입

--- src/main/java/com/fisa/kafrika_backend/common/config/ChatRoomInitializer.java
+++ src/main/java/com/fisa/kafrika_backend/common/config/ChatRoomInitializer.java
@@ -3,6 +3,7 @@ package com.fisa.kafrika_backend.common.config;
 import com.fisa.kafrika_backend.entity.ChatRoom;
 import com.fisa.kafrika_backend.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,14 +12,17 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ChatRoomInitializer implements CommandLineRunner {
 
+    @Value("${chat.room.default-name}")
+    private String defaultRoomName;
+
     private final ChatRoomRepository chatRoomRepository;
 
     @Override
     @Transactional
     public void run(String... args) {
-        if (chatRoomRepository.findByName("default-chatroom").isEmpty()) {
+        if (chatRoomRepository.findByName(defaultRoomName).isEmpty()) {
             chatRoomRepository.save(ChatRoom.builder()
-                    .name("default-chatroom")
+                    .name(defaultRoomName)
                     .build());
             System.out.println("디폴트 채팅방 생성 완료");
         }
