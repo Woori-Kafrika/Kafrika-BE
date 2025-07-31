579f719feb5d29531b06d1513454db6f22041e8e
hyunn522 <tjgus52213@gmail.com>
2025-08-01 00:46:10 +0900

    feat: 카프카를 통한 메시지 처리 로직 추가

---
+++ src/main/java/com/fisa/kafrika_backend/service/ChatKafkaListener.java
@@ -0,0 +1,57 @@
+package com.fisa.kafrika_backend.service;
+
+import static com.fisa.kafrika_backend.common.config.ChatSetInitializer.DEFAULT_CHATROOM;
+import static com.fisa.kafrika_backend.common.response.status.BaseExceptionResponseStatus.CHATROOM_NOT_FOUND;
+import static com.fisa.kafrika_backend.common.response.status.BaseExceptionResponseStatus.USER_NOT_FOUND;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fisa.kafrika_backend.common.exception.CustomException;
+import com.fisa.kafrika_backend.dto.ChatMessageRequest;
+import com.fisa.kafrika_backend.entity.ChatMessage;
+import com.fisa.kafrika_backend.entity.ChatRoom;
+import com.fisa.kafrika_backend.entity.User;
+import com.fisa.kafrika_backend.repository.ChatRoomRepository;
+import com.fisa.kafrika_backend.repository.ChattingRepository;
+import com.fisa.kafrika_backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ChatKafkaListener {
+
+    private final SimpMessagingTemplate messagingTemplate;
+    private final ObjectMapper objectMapper;
+    private final ChattingRepository chattingRepository;
+    private final ChatRoomRepository chatRoomRepository;
+    private final UserRepository userRepository;
+
+    @KafkaListener(topics = "chat-message", groupId = "chat-consumer-group")
+    public void consume(String message) {
+        try {
+            System.out.println("consume 진입");
+            ChatMessageRequest chatMessageRequest = objectMapper.readValue(message, ChatMessageRequest.class);
+
+            ChatRoom chatRoom = chatRoomRepository.findByName(DEFAULT_CHATROOM)
+                    .orElseThrow(() -> new CustomException(CHATROOM_NOT_FOUND));
+
+            User user = userRepository.findById(chatMessageRequest.getUserId())
+                    .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
+
+            chattingRepository.save(ChatMessage.builder()
+                            .detailMessage(chatMessageRequest.getMessage())
+                            .chatRoom(chatRoom)
+                            .user(user)
+                            .build());
+
+            messagingTemplate.convertAndSend("/sub/chat", chatMessageRequest);
+            log.info("브로드캐스트 완료");
+        } catch (Exception e) {
+            log.error("Kafka 처리 실패", e);
+        }
+    }
+}
